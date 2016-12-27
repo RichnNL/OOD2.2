a1f3c62e58395060d220d9ea16945643c15590a3
Dyer,Richard R.J <r.dyer@student.fontys.nl>
2016-12-27 12:29:30 +0100

    Can Place Item

--- OOD2/OOD2/PipelineNetwork.cs
+++ OOD2/OOD2/PipelineNetwork.cs
@@ -13,10 +13,14 @@ namespace OOD2
         private Item selectedItem;
         private Item selectedItem2;
         private List<Item> items;
+        private int ComponentSize;
+        private int pipelineWidth;
         SaveLoad fileHandler;
         public PipelineNetwork()
         {
             items = new List<Item>();
+            ComponentSize = 20;
+            pipelineWidth = 5;
         }
         public bool addComponent(Item i, Point pos)
         {
@@ -64,6 +68,27 @@ namespace OOD2
         }
         private bool CanPlaceItem(Point pos)
         {
+            foreach(Item i in items)
+            {
+                if(i.GetType() == typeof(Component))
+                {
+                    // The item is a component 
+                    if (ComponentInPosition((Component)i, pos))
+                    {
+                        return false;
+                    }
+
+                }
+                else
+                {
+                    //the item is a pipeline
+                    if (PipelineInPosition((Pipeline)i, pos))
+                        {
+                        return false;
+                        }
+
+                }
+            }
             return true;
         }
         public bool ReplaceComponent(Item i)
@@ -78,13 +103,87 @@ namespace OOD2
         {
 
         }
-        private Point calEnterPoint(Item i, Item i2)
+       
+        private bool ComponentInPosition(Component c , Point pos)
         {
+            Point topLeft = c.getPosition();
+            Point bottomRight = c.getPosition();
+            topLeft.X -= (ComponentSize / 2);
+            topLeft.Y -= (ComponentSize / 2);
+            bottomRight.X += (ComponentSize / 2);
+            bottomRight.Y += (ComponentSize / 2);
+            if (topLeft.X >= pos.X && bottomRight.X <= pos.X)
+            {
+                if (topLeft.Y >= pos.Y && bottomRight.Y <= pos.Y)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool PipelineInPosition(Pipeline p ,Point pos)
+        {
+            //First need to get the four points of the pipeline
+            Point slope = new Point();
+            slope.Y = p.output.getPosition().Y - p.input.getPosition().Y;
+            slope.X = p.output.getPosition().X - p.input.getPosition().X;
+            Point temp = slope;
+            // Get the perpendicular slope
+            slope.Y = -(slope.X);
+            slope.X = -(temp.Y);
+            Point A = new Point(); //Top Left
+            Point B = new Point(); //Top Right
+            Point C = new Point(); //Bottom Right
+            Point D = new Point(); //Bottom Left
+            A.X = p.input.getPosition().X + (slope.X * (pipelineWidth / 2));
+            A.Y = p.input.getPosition().Y + (slope.Y * (pipelineWidth / 2));
+            B.X = p.input.getPosition().X - (slope.X * (pipelineWidth / 2));
+            B.Y = p.input.getPosition().Y - (slope.Y * (pipelineWidth / 2));
+            D.X = p.output.getPosition().X + (slope.X * (pipelineWidth / 2));
+            D.Y = p.output.getPosition().Y - (slope.Y * (pipelineWidth / 2));
+            C.X = p.output.getPosition().X + (slope.X * (pipelineWidth / 2));
+            C.Y = p.output.getPosition().Y - (slope.Y * (pipelineWidth / 2));
+
+            double PipelineArea = getArea(A, B, C, D);
+            double APD = getArea(A, pos, D);
+            double DPC = getArea(D, pos, C);
+            double CPB = getArea(C, pos, B);
+            double PBA = getArea(pos, B, A);
+
+            double PointArea = APD + DPC + CPB + PBA;
+            if(PointArea < PipelineArea)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
 
         }
-        private Point calExitPoint(Item i, Item i2)
+        private void getDistance(Point A, Point B, Point C, out double ab, out double ac , out double bc)
         {
-
+            ab = Math.Sqrt(Math.Pow((B.X - A.X), 2) + Math.Pow((B.Y - A.Y), 2));
+            ac = Math.Sqrt(Math.Pow((C.X - A.X), 2) + Math.Pow((C.Y - A.Y), 2));
+            bc = Math.Sqrt(Math.Pow((C.X - B.X), 2) + Math.Pow((C.Y - B.Y), 2));
+        }
+        private double getArea(Point A, Point B, Point C)
+        {
+            double ab, ac, bc;
+            getDistance(A, B, C,out ab, out ac, out bc);
+            //Herons formula
+            double s = (ab + ac + bc) / 2;
+            double area = Math.Pow((s*((s - ab)*(s - ac)*(s - bc))), 2);
+            return area;
+        }
+        private double getArea(Point A,Point B, Point C,Point D)
+        {
+            double width = Math.Sqrt(Math.Pow((B.X - A.X), 2) + Math.Pow((B.Y - A.Y), 2));
+            double length = Math.Sqrt(Math.Pow((C.X - A.X), 2) + Math.Pow((C.Y - A.Y), 2));
+            return width * length;
         }
     }
 }
